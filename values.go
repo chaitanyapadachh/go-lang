25e7a0c2624ee2520e4de566621bbf7d6bf8bcfe
Chaitanya Padachh <chaitanyapadachh19@gmail.com>
2024-10-12 07:58:21 +0530

    Values

---
+++ values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("go" + "lang") //Strings are added together using +
+	fmt.Println("1+1 =", 1+1)  //Numbers and Floats
+	fmt.Println("7.0/3.0 = ", 7.0/3.0)
+	fmt.Println(true && false) //Operators and Boolens
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+}
